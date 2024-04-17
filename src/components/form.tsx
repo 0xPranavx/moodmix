6ac9e060cc30ad7c246336ca2d5e8c6977b8ea0f
inprovert <92786046+inprovert@users.noreply.github.com>
2024-04-17 22:58:09 +0530

    render issue

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -231,7 +231,7 @@ const MultipleSelectorWithForm = () => {
 }
 
   {playlist && 
-   <div className='flex justify-center items-center m-10 w-full'>
+   <div className='flex justify-center items-center m-8 w-full'>
 <iframe className="rounded-xl" src={`https://open.spotify.com/embed/playlist/${playlist}?utm_source=generator&theme=0`} width="100%" height="352" frameBorder="0"  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
 </div>
   
