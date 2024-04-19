906293e53d7a324ed357b36b139aabcc1c83c324
inprovert <92786046+inprovert@users.noreply.github.com>
2024-04-19 05:34:20 +0530

    margin bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "@/components/ui/toaster"
 import { firaSans} from "@/app/font";
 import Image from "next/image";
 import Logo from '@/components/icon'
+import Link from "next/link";
 
 
 
@@ -25,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       
     <body className= {`${firaSans.className} flex h-screen flex-col  m-2 justify-between`}>
-      <nav className="flex justify-start items-center mt-2 m-4  pl-2">
+      <nav className="flex justify-start items-center mt-2 m-2 ">
        <Logo width="40px" height="40px"/>
        <h1 className="text-xl font-bold mr-2">moodmix</h1>
       </nav>
@@ -33,7 +34,7 @@ export default function RootLayout({
       <Toaster /></div>
       <footer className="flex justify-start items-center p-4 w-full">
       
-       <p className="text-md">Made with ♥ by 0xpranavx.</p>
+       <p className="text-md">Made with ♥ by <Link className="text-blue-400" href="https://x.com/0xPranavx">0xpranavx</Link>.</p>
       </footer>
    
       </body>
